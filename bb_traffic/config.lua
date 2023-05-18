85e62f982e1f2c1acb6a3d1a5ce7fa13f61f7565
BlackBear <111135227+BllackBear@users.noreply.github.com>
2023-05-18 04:08:42 +0300

    Create config.lua

---
+++ bb_traffic/config.lua
@@ -0,0 +1,7 @@
+Config = {}
+
+-- Traffic density value (0.0 - Removes all AI, 0.5 - Only half remains, 1.0 - Normal density)
+Config.TrafficDensity = 0.0
+
+-- Pedestrian density value (0.0 - Removes all AI, 0.5 - Only half remains, 1.0 - Normal density)
+Config.PedDensity = 0.0
