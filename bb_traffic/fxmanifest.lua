572b2ca8454dc48580295ecb14b249ae93c1a448
BlackBear <111135227+BllackBear@users.noreply.github.com>
2023-05-18 04:09:39 +0300

    Update fxmanifest.lua

--- bb_traffic/fxmanifest.lua
+++ bb_traffic/fxmanifest.lua
@@ -4,9 +4,10 @@ game 'gta5'
 name "bb_traffic"
 description "Simple Traffic Controller"
 author "BlackBear"
-version "1.0.0"
+version "1.0.1"
 lua54 'yes'
 
 client_scripts{
     'client.lua',
+    'config.lua'
 }
